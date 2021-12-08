021d0423d0285cdccc4bac2ce4aaed5f6e28eec4
slavaider <slavaider1@gmail.com>
2021-12-08 18:32:02 +0500

    remove react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React from "react";
 
 import store from "@store/store";
 import { Provider } from "react-redux";
-import { Redirect, Route, Switch } from "react-router-dom";
 
 import PersonEditorPage from "./pages/PersonEditorPage";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <Switch>
-        <Route path="/" exact component={PersonEditorPage} />;
-        <Redirect from="*" to="/repos" />
-      </Switch>
+      <PersonEditorPage />
     </Provider>
   );
 };
